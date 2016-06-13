02fafb50715ede58c3675c0f01f6c2bc0d2b924b
Ansh <lightnarcissus@gmail.com>
2016-06-13 13:32:51 -0400

    Testing Selenium on Firefox via Gecko

--- loophttp.py
+++ loophttp.py
@@ -1,6 +1,6 @@
 import pyshark
  
-cap = pyshark.LiveCapture('Wi-Fi')
+cap = pyshark.LiveCapture('Wi-Fi', display_filter='http')
  
 def print_conversation_header(pkt):
     try:
@@ -11,7 +11,12 @@ def print_conversation_header(pkt):
         src_port = pkt[pkt.transport_layer].srcport
         dst_addr = pkt.ip.dst
         dst_port = pkt[pkt.transport_layer].dstport
-        print 'A human who went by the title of %s , residing at %s , looked out the window %s, to talk to %s, who lived at %s via the ancient mystic language of %s'  % (http_user, src_addr, src_port, http_host, dst_addr, protocol)
+        print pkt.tcp.checksum
+        #temporarily disabled
+        # if protocol == 'UDP':
+        #     print 'A human who went by the title of %s , residing at %s , looked out the window marked with number %s, to talk to %s, who lived at %s via the ancient mystic language of %s'  % (http_user, src_addr, src_port, http_host, dst_addr, protocol)
+        # else:
+        #     print 'When %s heard a voice at %s , they could not help but look out of the window marked with number %s and thought of %s, who they had met a long time back at %s where they had together learnt the art of speaking in %s'  % (http_user, src_addr, src_port, http_host, dst_addr, protocol)     
     except AttributeError as e:
         #ignore packets that aren't TCP/UDP or IPv4
         pass
